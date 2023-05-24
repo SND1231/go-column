492f426a2588df8478cf805905852eb308fdcd5e
Sanada.Y <villa7iniesta8@gmail.com>
2023-05-24 22:15:54 +0900

    add: cobra, viperの導入

---
+++ src/cmd/root.go
@@ -0,0 +1,66 @@
+/*
+Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"log"
+	"os"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	Type     string
+	Host     string
+	Port     int
+	User     string
+	Password string
+	Name     string
+}
+
+var config Config
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use: "go-column",
+	Run: func(cmd *cobra.Command, args []string) {
+		// configの中身を出力
+		log.Printf("configの中身:{type: %s, host: %s, port: %d, user: %s, pass: %s, name: %s}",
+			config.Type, config.Host, config.Port, config.User, config.Password, config.Name)
+	},
+}
+
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	// 初期化処理
+	// configの設定
+	rootCmd.Flags().StringP("configName", "n", "default.toml", "config file name")
+
+	// Runを実行するたびに、initConfigを呼び出す。その後に、Runの処理が動き出す。
+	cobra.OnInitialize(initConfig)
+}
+
+func initConfig() {
+	configName, _ := rootCmd.Flags().GetString("configName")
+	viper.SetConfigFile(configName)
+
+	// 設定ファイルを読み込む
+	if err := viper.ReadInConfig(); err != nil {
+		log.Println(err)
+		os.Exit(1)
+	}
+
+	// 設定ファイルの内容を構造体に設定
+	if err := viper.Unmarshal(&config); err != nil {
+		log.Println(err)
+		os.Exit(1)
+	}
+}
---
+++ src/main.go
@@ -0,0 +1,11 @@
+/*
+Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+
+*/
+package main
+
+import "github.com/SND1231/go-column/cmd"
+
+func main() {
+	cmd.Execute()
+}
