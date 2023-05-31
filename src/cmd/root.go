9e83c9308ba0f3df09fbabae8ad3c6e996479ea3
Sanada.Y <villa7iniesta8@gmail.com>
2023-05-31 22:53:28 +0900

    add: APIサーバーを追加

--- src/cmd/root.go
+++ src/cmd/root.go
@@ -5,8 +5,10 @@ package cmd
 
 import (
 	"log"
+	"net/http"
 	"os"
 
+	"github.com/SND1231/go-column/router"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
@@ -22,13 +24,22 @@ type Config struct {
 
 var config Config
 
-// rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use: "go-column",
 	Run: func(cmd *cobra.Command, args []string) {
 		// configの中身を出力
 		log.Printf("configの中身:{type: %s, host: %s, port: %d, user: %s, pass: %s, name: %s}",
 			config.Type, config.Host, config.Port, config.User, config.Password, config.Name)
+
+		// サーバーの設定
+		r := router.Get()
+		srv := &http.Server{
+			Addr:    ":3020",
+			Handler: r,
+		}
+
+		// サーバーの起動
+		srv.ListenAndServe()
 	},
 }
 
---
+++ src/handler/user.go
@@ -0,0 +1,139 @@
+package handler
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/mholt/binding"
+)
+
+type UserHandler struct{}
+
+func NewUserHandler() *UserHandler {
+	return &UserHandler{}
+}
+
+// 失敗時のレスポンスの設定
+func setErrorResponse(w http.ResponseWriter, status int) http.ResponseWriter {
+	// レスポンスのヘッダ設定
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+
+	// エラーだったらステータス500で返す
+	//w.WriteHeader(http.StatusInternalServerError)
+	w.WriteHeader(status)
+	return w
+}
+
+// 成功時のレスポンスの設定
+func setSuccessResponse(w http.ResponseWriter, res []byte) http.ResponseWriter {
+	// レスポンスの内容をjsonに変換
+	w.Write(res)
+	return w
+}
+
+// ユーザーの作成APIのリクエスト
+type AddUserInput struct {
+	Name string
+	Age  int
+}
+
+// リクエストのマッピング。ポインターレシーバーにすること
+func (input *AddUserInput) FieldMap(r *http.Request) binding.FieldMap {
+	return binding.FieldMap{
+		&input.Name: "name",
+		&input.Age:  "age",
+	}
+}
+
+// ユーザーの作成APIのレスポンス
+type AddUserOutput struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+	Age  int    `json:"age"`
+}
+
+// ユーザーの作成API
+func (h *UserHandler) Add(w http.ResponseWriter, r *http.Request) {
+	var err error
+	var berr binding.Errors
+	var response *AddUserOutput
+	var res []byte
+
+	// request -> AddUserInput型に変換
+	var request AddUserInput
+	berr = binding.Bind(r, &request)
+	if berr != nil {
+		log.Println(berr)
+		setErrorResponse(w, http.StatusInternalServerError)
+		return
+	}
+
+	// responseの作成
+	response = &AddUserOutput{
+		ID:   1,
+		Name: request.Name,
+		Age:  request.Age,
+	}
+
+	// レスポンスをjsonに変換
+	res, err = json.Marshal(response)
+	if err != nil {
+		log.Println(err)
+		setErrorResponse(w, http.StatusInternalServerError)
+		return
+	}
+	setSuccessResponse(w, res)
+}
+
+// ユーザーの詳細取得APIのリクエスト
+type GetUserInput struct {
+	ID int
+}
+
+// リクエストのマッピング。ポインターレシーバーにすること
+func (input *GetUserInput) FieldMap(r *http.Request) binding.FieldMap {
+	return binding.FieldMap{
+		&input.ID: "id",
+	}
+}
+
+// ユーザーの詳細取得APIのレスポンス
+type GetUserOutput struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+	Age  int    `json:"age"`
+}
+
+// ユーザーの詳細取得API
+func (h *UserHandler) Get(w http.ResponseWriter, r *http.Request) {
+	var err error
+	var berr binding.Errors
+	var response *GetUserOutput
+	var res []byte
+
+	// request -> GetUserInput型に変換
+	var request GetUserInput
+	berr = binding.Bind(r, &request)
+	if berr != nil {
+		log.Println(berr)
+		setErrorResponse(w, http.StatusInternalServerError)
+		return
+	}
+
+	// responseの作成
+	response = &GetUserOutput{
+		ID:   request.ID,
+		Name: "Jony",
+		Age:  45,
+	}
+
+	// レスポンスをjsonに変換
+	res, err = json.Marshal(response)
+	if err != nil {
+		log.Println(err)
+		setErrorResponse(w, http.StatusInternalServerError)
+		return
+	}
+	setSuccessResponse(w, res)
+}
