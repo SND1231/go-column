041eb7ccc9cf8dd64e3a8ec6e28bd3b9df0cfd72
Sanada.Y <villa7iniesta8@gmail.com>
2023-12-28 21:52:17 +0900

    feat: middlewareでDBの設定を見れるようにした

--- src/router/router.go
+++ src/router/router.go
@@ -1,6 +1,9 @@
 package router
 
 import (
+	"log"
+	"net/http"
+
 	"github.com/SND1231/go-column/handler"
 	"github.com/SND1231/go-column/setting"
 	"github.com/go-chi/chi"
@@ -11,11 +14,26 @@ func Get(dbSetting setting.DB) *chi.Mux {
 
 	// ハンドラーの初期化
 	userHandler := handler.NewUserHandler(dbSetting)
+	printDBsetting := makePrintDbSetting(dbSetting)
 
+	r.Use(printDBsetting)
 	// httpルーティング
 	r.Route("/user", func(r chi.Router) {
 		r.Post("/add", userHandler.Add)
 		r.Get("/detail", userHandler.Get)
 	})
 	return r
+}
+
+func makePrintDbSetting(dbSetting setting.DB) func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return print(next, dbSetting)
+	}
+}
+
+func print(next http.Handler, dbSetting setting.DB) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("type:%s, host:%s", dbSetting.Type, dbSetting.Host)
+		next.ServeHTTP(w, r)
+	})
 }
